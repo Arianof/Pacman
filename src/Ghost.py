c5a69e929274747359f12da976f40eb681952079
Arianof <Arian.fathi03@gmai.com>
2023-11-19 12:35:20 +0330

    feat: possible_moves function has been developed to determine next possible moves of ghosts

--- src/Ghost.py
+++ src/Ghost.py
@@ -18,38 +18,38 @@ class Ghost:
             self.move_left(game_map)
 
     def move_up(self, game_map):
-        if game_map[self.location[0] - 1][self.location[1]] == 'X':
+        if game_map[self.location[0] - 1][self.location[1]] == 0:
             return
         else:
             self.location[0] -= 1
 
     def move_right(self, game_map):
-        if game_map[self.location[0]][self.location[1] + 1] == 'X':
+        if game_map[self.location[0]][self.location[1] + 1] == 0:
             return
         else:
             self.location[1] += 1
 
     def move_down(self, game_map):
-        if game_map[self.location[0] + 1][self.location[1]] == 'X':
+        if game_map[self.location[0] + 1][self.location[1]] == 0:
             return
         else:
             self.location[0] += 1
 
     def move_left(self, game_map):
-        if game_map[self.location[0]][self.location[1] - 1] == 'X':
+        if game_map[self.location[0]][self.location[1] - 1] == 0:
             return
         else:
             self.location[1] -= 1
 
     def possible_moves(self, game_map):
         res = []
-        if game_map[self.location[0]][self.location[1] - 1] == ' ':
+        if game_map[self.location[0]][self.location[1] - 1] != 0:
             res.append([self.location[0], self.location[1] - 1])
-        if game_map[self.location[0] - 1][self.location[1]] == ' ':
+        if game_map[self.location[0] - 1][self.location[1]] != 0:
             res.append([self.location[0] - 1, self.location[1]])
-        if game_map[self.location[0]][self.location[1] + 1] == ' ':
+        if game_map[self.location[0]][self.location[1] + 1] != 0:
             res.append([self.location[0], self.location[1] + 1])
-        if game_map[self.location[0] + 1][self.location[1]] == ' ':
+        if game_map[self.location[0] + 1][self.location[1]] != 0:
             res.append([self.location[0] + 1, self.location[1]])
         return res
 
