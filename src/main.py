d26794b6fd92d570eb7e09e259605021c59fe61c
Arianof <Arian.fathi03@gmai.com>
2023-11-19 12:36:44 +0330

    fix: main.py for runnig the game has been developed

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from Game import Game
 from Ghost import Ghost
 from Pacman import Pacman
 if __name__ == '__main__':
-    ghost1 = Ghost(4, 10)
+    ghost1 = Ghost(4, 3)
     ghost2 = Ghost(6, 2)
     pacman = Pacman()
     g = Game(ghost1, ghost2, pacman)
@@ -12,9 +12,7 @@ if __name__ == '__main__':
         ghost1.move(g.game_map)
         ghost2.move(g.game_map)
         pacman.move(g.minimax)
-        print(ghost1.location)
-        print(ghost2.location)
-        print(pacman.location)
+        print(g.count_remained_dots())
         g.print_game_map()
 
 
