ae61e1e2faff57ba98593ea3618a6fcba7ed84fc
Arianof <Arian.fathi03@gmai.com>
2023-11-19 12:36:00 +0330

    feat: Possible moves function has been developed and minimax function has changed

--- src/Pacman.py
+++ src/Pacman.py
@@ -3,17 +3,17 @@ class Pacman:
         self.location = [9, 10]
 
     def move(self, minimax):
-        best_score, self.location = minimax(0, 0, 4)
+        best_score, self.location = minimax(0, 0, 1, 0)
         print(best_score)
 
     def possible_moves(self, game_map):
         res = []
-        if game_map[self.location[0]][self.location[1] - 1] == ' ':
+        if game_map[self.location[0]][self.location[1] - 1] != 0:
             res.append([self.location[0], self.location[1] - 1])
-        if game_map[self.location[0] - 1][self.location[1]] == ' ':
+        if game_map[self.location[0] - 1][self.location[1]] != 0:
             res.append([self.location[0] - 1, self.location[1]])
-        if game_map[self.location[0]][self.location[1] + 1] == ' ':
+        if game_map[self.location[0]][self.location[1] + 1] != 0:
             res.append([self.location[0], self.location[1] + 1])
-        if game_map[self.location[0] + 1][self.location[1]] == ' ':
+        if game_map[self.location[0] + 1][self.location[1]] != 0:
             res.append([self.location[0] + 1, self.location[1]])
         return res
