ddb260bcc41fe9dfc6f0d1a6a49619b01e8e28be
Arianof <Arian.fathi03@gmai.com>
2023-11-23 17:59:18 +0330

    feat: minor bugs fixed and expectimax function has been added

--- src/Pacman.py
+++ src/Pacman.py
@@ -2,9 +2,17 @@ class Pacman:
     def __init__(self):
         self.location = [9, 10]
 
-    def move(self, minimax):
-        best_score, self.location = minimax(0, 0, 1, 0)
-        print(best_score)
+    def move(self, minimax, manhattan_dis_calc, expectimax):
+        manhattan_dis = manhattan_dis_calc()
+        alpha = -1000000000
+        beta = 100000000
+        #res = minimax(0, 0, 4, 0, manhattan_dis, alpha, beta)
+        res = expectimax(0, 0, 1, 0, manhattan_dis)
+        if type(res) is int:
+            best_score = res
+        else:
+            best_score, self.location = res
+        #print(best_score)
 
     def possible_moves(self, game_map):
         res = []
